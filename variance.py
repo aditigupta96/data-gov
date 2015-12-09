6e25dd1669b806ae0d99d3bb29abd7150d4f487d
Aditi <aditi.medha96@gmail.com>
2015-12-09 20:33:36 +0530

    variance

--- variance.py
+++ variance.py
@@ -1,11 +1,12 @@
 def mean(a):
-    return float(sum(a) / len(a))
+    return float(sum(a)) / len(a)
 
 def variance(a):
     x = mean(a)
-    variance = sum([(i-x)**2 for i in a])
+    variance = sum([(i-x)**2 for i in a])/len(a)
     return float(variance)
 
 y = list(map(int,input().split()))
 z= variance(y)
+print(x)
 print(z)
